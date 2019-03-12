9a3f7d139fbf7b9bfa054ea35782a5a205c19d0c
RichFisher <rich.fishernc@gmail.com>
2019-03-11 20:15:53 -0500

    neptune and uranus are dumb

--- spaceshipPlanets/Program.cs
+++ spaceshipPlanets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace spaceshipPlanets
 {
@@ -6,7 +7,40 @@ namespace spaceshipPlanets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> planetList2 = new List<string>() { "Jupiter", "Saturn" };
+            planetList.AddRange(planetList2);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+
+            var probes = new Dictionary<string, List<string>>();
+            probes.Add("Bob the probe", new List<string> { "Mercury", "Mars"});
+            probes.Add("Tediousness", new List<string> { "Earth", "Mars", "Jupiter", "Saturn" });
+            probes.Add("Apathy", new List<string> { "Jupiter", "Uranus" });
+
+            List<string> probesNStuff = new List<string>();
+            foreach (var planet in planetList)
+            {
+                string probesVisited = planet + ": ";
+                foreach (var (probe, planets) in probes)
+                {
+                    if (planets.Contains(planet))
+                    {
+                        probesVisited += probe + " ";
+                    }
+                }
+                probesNStuff.Add(probesVisited);
+            }
+            foreach(var probeList in probesNStuff)
+            {
+                Console.WriteLine(probeList);
+            }
+            Console.ReadLine();
         }
     }
 }
