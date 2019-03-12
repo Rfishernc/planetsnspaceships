b51b0dca3d608346c821f401eb3de4f2d5e890b1
RichFisher <rich.fishernc@gmail.com>
2019-03-11 19:51:44 -0500

    Add project files.

---
+++ spaceshipPlanets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spaceshipPlanets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
